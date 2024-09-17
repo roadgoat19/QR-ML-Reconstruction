289f66017d4763a74cf56ca5a42315ccdc55c31a
roadgoat19 <123213767+roadgoat19@users.noreply.github.com>
2024-09-17 16:49:01 -0400

    trained first model

--- gen_set.py
+++ gen_set.py
@@ -1,17 +1,18 @@
 import gen_qr as generator
 import uuid
 from qrcodegen import *
+import random, string
 from scipy import fft
 
 def generate_ecl_set(set_size, ecl):
-    for _ in range(set_size):
-        message = str(uuid.uuid1())[:15] # random hexcode as message (for now)
+    for i in range(set_size):
+        message = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+        # message = str(uuid.uuid1())[:15] # random hexcode as message (for now)
         qr = generator.generate_QR(message, ecl, -1, 29)
-        transform = fft2(qr)
         generator.file_output(qr, message, ecl)
 
 def main():
-    iterations = 1000
+    iterations = 10000
     for ecl in ["low", "med", "quart", "high"]:
         print(ecl)
         generate_ecl_set(iterations//4, ecl)
